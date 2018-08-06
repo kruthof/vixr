ab87d03d24d8bd530a4552bc902bcdfdd2b4de97
Garvin <garvin@Garvins-MacBook-Pro.local>
2018-08-06 21:14:16 +0200

    adding smoother functions

--- R/app.R
+++ R/app.R
@@ -14,9 +14,20 @@ ui <- shinyUI(fluidPage(
 
   sidebarLayout(
     sidebarPanel(
-      sliderInput("MovingAvg", label = "Simple Moving Average Smoother",
-                  value = 60, min = 2, max = 129, step = 1),
-      checkboxInput("showgrid", label = "Show Grid", value = TRUE)
+      checkboxInput(inputId = "showgrid", label = "Show Grid", value = TRUE),
+      checkboxInput(inputId = "addsmoother", label = "Add Smoother", value = FALSE),
+
+      conditionalPanel(condition = ("input.addsmoother==true"),
+      selectInput("smoother",
+                  "Smoother type",
+                  choices = list("Linear regression" = "lm",
+                                 "Loess smooth" = "loess",
+                                 "Spline smooth" = "smooth.spline",
+                                 "Moving Average" = "ma"),
+                  selected = "loess")),       
+      conditionalPanel(condition = ("input.addsmoother==true && input.smoother=='ma'"), sliderInput("MovingAvg", label = "Simple Moving Average Smoother",
+                                                                                                                                           value = 60, min = 2, max = 129, step = 1))
+      
 
     ),
     mainPanel(
@@ -29,17 +40,35 @@ ui <- shinyUI(fluidPage(
 
 
 server <- function(input, output, session) {
-  predicted <- reactive({
-    hw <- cbind("vix" = as.ts(vix2shiny$VIX), "ma" = ma(as.ts(vix2shiny$VIX),input$MovingAvg))
+  
 
+    predicted <- reactive({
+    if(input$addsmoother) {
+      if(input$smoother=="ma") {
+        vix_smoother = ma(as.ts(vix2shiny$VIX),input$MovingAvg) 
+        } else if(input$smoother=="smooth.spline") {
+          fit = fitVIX(vix2shiny,fit_type = "smooth.spline") 
+          vix_smoother = predict(fit$model,data.frame(x=seq(1:length(vix2shiny$VIX))))
+          vix_smoother = vix_smoother$y$x
+          }else{  
+      fit = fitVIX(vix2shiny,fit_type = input$smoother) 
+      vix_smoother = predict(fit$model,data.frame(x=seq(1:length(vix2shiny$VIX))))
+      }
+      }
+    else{vix_smoother= as.ts(vix2shiny$VIX)}
+    hw <- cbind("vix" = as.ts(vix2shiny$VIX), "smooth" = vix_smoother)
+    
   })
 
   output$dygraph <- renderDygraph({
-    dygraph(predicted(), main = "VIX Intraday Prices by Minutes") %>%
-      dySeries("vix", label = "VIX")%>%dySeries("ma", label = "Moving Average",col="red")%>% 
 
+    dygraph(predicted(), main = "VIX Intraday Prices by Minutes") %>%
+      dySeries("vix", label = "VIX")%>% 
+      dySeries("smooth", label = "Smoother",col="blue")%>% 
       dyRangeSelector()%>%
       dyOptions(drawGrid = input$showgrid)
+
+    
 
   })
   }
