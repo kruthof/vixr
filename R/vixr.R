75eaf502154d5596d5f02d854255ddf311ffb049
Garvin <garvin@Garvins-MacBook-Pro.local>
2018-08-16 18:33:43 +0200

    documentation

---
+++ R/vixr.R
@@ -0,0 +1,9 @@
+#' vixr: A package for replicating the VIX
+#'
+#' The vixr package contains four categories of important functions. For more details, please consult the vignette.
+#' 
+#' @seealso \code{\link{vix_calc}, \link{fitVIX},\link{plotvix},\link{letVixShiny}} 
+#'
+#' @docType package
+#' @name vixr
+NULL
